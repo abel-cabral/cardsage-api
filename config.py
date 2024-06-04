3ea5ff92841011ec3402b8876f28b6b6ca795a30
C1333760 Abel Cabral De Arruda <c1333760@interno.bb.com.br>
2024-06-04 10:57:16 -0300

    token de acesso valido por 30 dias

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 import os
+from datetime import timedelta
+
 class Config:
     DEBUG = True
     SECRET_KEY = os.getenv('SECRET_KEY')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES', 30)))
     # Adicione outras configurações aqui, se necessário
+    
+__all__ = ['Config']
