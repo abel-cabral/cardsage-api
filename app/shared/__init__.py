a4c782e6700b9f53ce317ed101ecba2713daed9f
C1333760 Abel Cabral De Arruda <c1333760@interno.bb.com.br>
2024-06-14 23:24:38 -0300

    (feat): rotas de inserir recebendo html text

--- app/shared/__init__.py
+++ app/shared/__init__.py
@@ -1,4 +1,3 @@
 from .mongodb import adicionar_ramo, collection, todos_ramos, deletar_ramo_por_id
 from .util import purificarHTML, partirHTML
 from .chatgpt import iniciarConversa, classificarTagsGerais, get_chatgpt_response
-from .selenium_html_extractor import html_extrator
